a5f360b99c95a455e4d0b305507b3aebbede2740
Shraddhachandel <shraddhachandel1997@gmail.com>
2023-12-03 17:04:22 +0530

    Video Card for Body

---
+++ src/Utils/constaints.js
@@ -0,0 +1,4 @@
+export const GOOGLE_API_KEY = "AIzaSyCxvKIIbQNlmqpOl3nxXIRI71DmE5J8aMQ"
+
+export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=60&regionCode=IN&key='
+ + GOOGLE_API_KEY;
